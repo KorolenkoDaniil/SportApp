650b3407a991b6de8ab1ce558af80b400de673fc
Korolenko Danila <korolenko.danila1@gmail.com>
2024-12-18 15:06:28 +0300

    Rankings after restructuring

---
+++ app/src/main/java/com/example/sportapp/Widgets/Matches/rankigs/rankingsParts/TeamPart.kt
@@ -0,0 +1,43 @@
+package com.example.sportapp.widgets.matches.rankigs.rankingsParts
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.sportapp.domain.RankingEntity
+
+@Composable
+fun TeamPart (modifier: Modifier, item: RankingEntity, index: Int){
+
+    val painter = rememberAsyncImagePainter(item.logoUrl)
+
+    Box(
+        modifier = modifier
+    ) {
+        Row (
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text((index + 1).toString(), modifier = Modifier.padding(end = 8.dp))
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(50.dp)
+            )
+            Text(item.shortName, modifier = Modifier.padding(end = 8.dp))
+        }
+    }
+}
