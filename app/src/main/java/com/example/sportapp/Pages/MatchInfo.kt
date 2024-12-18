19a74920667a3c1e5cf4dac888c1f296d36482a2
Korolenko Danila <korolenko.danila1@gmail.com>
2024-12-18 16:10:03 +0300

    I have added MatchInfo

---
+++ app/src/main/java/com/example/sportapp/Pages/MatchInfo.kt
@@ -0,0 +1,9 @@
+package com.example.sportapp.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MatchInfo () {
+    Text(text = "MatchInfo")
+}
