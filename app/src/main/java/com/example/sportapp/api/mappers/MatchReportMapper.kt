145e934be67889496bff999c10247f7f823549cd
Korolenko Danila <korolenko.danila1@gmail.com>
2025-01-09 13:00:45 +0300

    stable version with displaying match report tree

--- app/src/main/java/com/example/sportapp/api/mappers/MatchReportMapper.kt
+++ app/src/main/java/com/example/sportapp/api/mappers/MatchReportMapper.kt
@@ -2,11 +2,12 @@ package com.example.sportapp.api.mappers
 
 import EventResponse
 import com.example.sportapp.domain.EventEntity
+import com.example.sportapp.domain.Replacement
 
 class MatchReportMapper {
 
     fun getMatchEventsList(matchEvents: List<EventResponse>): List<EventEntity> {
-        return matchEvents.map { event ->
+        val eventsList = matchEvents.map { event ->
             EventEntity(
                 type = event.type ?: "unknown",
                 matchPhase = event.matchPhase ?: "default_phase",
@@ -19,14 +20,46 @@ class MatchReportMapper {
                 playerFullName = event.playerFullName ?: "Unknown",
                 playerShirtName = event.playerShirtName ?: "Unknown",
                 minute = event.minute ?: 0,
-                additionalMinute = event.additionalMinute?: 0
+                additionalMinute = event.additionalMinute ?: 0,
+                replacement = Replacement("", "", "", "", "", "")
             )
         }
+
+        return eventMerging(eventsList)
     }
 
 
-//
-//    fun eventMerging(eventsList: List<EventEntity>) : List<EventEntity> {
-//
-//    }
+    private fun eventMerging(eventsList: List<EventEntity>): List<EventEntity> {
+
+        val groupedByOrder = eventsList.groupBy { it.order }
+
+        val mergedEvents = mutableListOf<EventEntity>()
+
+        for ((_, events) in groupedByOrder) {
+            val substitutionIn = events.find { it.type == "substitution_player_in" }
+            val substitutionOut = events.find { it.type == "substitution_player_out" }
+
+            if (substitutionIn != null && substitutionOut != null) {
+
+                val mergedEvent = substitutionIn.copy(
+                    type = "substitution_player",
+                    replacement = Replacement(
+                        teamId = substitutionOut.teamId,
+                        playerId = substitutionOut.playerId,
+                        playerSurname = substitutionOut.playerSurname,
+                        name = substitutionOut.name,
+                        playerFullName = substitutionOut.playerFullName,
+                        playerShirtName = substitutionOut.playerShirtName
+                    )
+                )
+
+                mergedEvents.add(mergedEvent)
+
+            } else {
+                mergedEvents.addAll(events)
+            }
+        }
+
+        return mergedEvents
+    }
 }
